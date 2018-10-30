2e078afa4485e17ffe36d41eec3798b12e1a3fd6
Carlos Carballo <carloscarballod@gmail.com>
2018-10-30 10:39:52 -0700

    TareaNorza-01
    Creacion del proyecto

---
+++ GeekGas/BaseDatos/Consumo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BaseDatos
+{
+    class Consumo
+    {
+
+        private int id;
+        private string rfc;
+        private string domicilio;
+        private string contrasena;
+        private string cuenta;
+        private int idUsuario;
+
+        public int IdUsuario
+        {
+            get { return idUsuario; }
+            set { idUsuario = value; }
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        private string nombre;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public string Cuenta
+        {
+            get { return cuenta; }
+            set { cuenta = value; }
+        }
+
+        public string Contrasena
+        {
+            get { return contrasena; }
+            set { contrasena = value; }
+        }
+
+        public string Rfc
+        {
+            get { return rfc; }
+            set { rfc = value; }
+        }
+
+        public string Domicilio
+        {
+            get { return domicilio; }
+            set { domicilio = value; }
+        }
+
+    }
+
+
+
+}
---
+++ GeekGas/BaseDatos/Usuario.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BaseDatos
+{
+    class Usuario
+    {
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        private string nombre;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        private string cuenta;
+
+        public string Cuenta
+        {
+            get { return cuenta; }
+            set { cuenta = value; }
+        }
+        private string contrasena;
+
+        public string Contrasena
+        {
+            get { return contrasena; }
+            set { contrasena = value; }
+        }
+        private string rfc;
+
+        public string Rfc
+        {
+            get { return rfc; }
+            set { rfc = value; }
+        }
+        private string domicilio;
+
+        public string Domicilio
+        {
+            get { return domicilio; }
+            set { domicilio = value; }
+        }
+    }
+}
