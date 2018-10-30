79bde57400a04e23cae2a427c0fae77b43db5752
Carlos Carballo <carloscarballod@gmail.com>
2018-10-30 12:33:51 -0700

    sdfd
    sdfsdfsd

--- GeekGas/GeekGas/ElegirConsumo.Designer.cs
+++ GeekGas/GeekGas/ElegirConsumo.Designer.cs
@@ -29,13 +29,14 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.dataGridView2 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.NOMBRE = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.IDCONSUMO = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.NOMBRES = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.IDCONSUMOS = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.NOMBRE = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.IDCONSUMO = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
@@ -52,6 +53,16 @@
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
+            // NOMBRES
+            // 
+            this.NOMBRES.HeaderText = "NOMBRES";
+            this.NOMBRES.Name = "NOMBRES";
+            // 
+            // IDCONSUMOS
+            // 
+            this.IDCONSUMOS.HeaderText = "IDCONSUMOS";
+            this.IDCONSUMOS.Name = "IDCONSUMOS";
+            // 
             // dataGridView2
             // 
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
@@ -62,6 +73,16 @@
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.Size = new System.Drawing.Size(283, 139);
             this.dataGridView2.TabIndex = 1;
+            // 
+            // NOMBRE
+            // 
+            this.NOMBRE.HeaderText = "NOMBRE";
+            this.NOMBRE.Name = "NOMBRE";
+            // 
+            // IDCONSUMO
+            // 
+            this.IDCONSUMO.HeaderText = "IDCONSUMO";
+            this.IDCONSUMO.Name = "IDCONSUMO";
             // 
             // button1
             // 
@@ -82,31 +103,21 @@
             this.label1.TabIndex = 3;
             this.label1.Text = "LISTA DE CONSUMO POR USUARIOS";
             // 
-            // NOMBRE
+            // button2
             // 
-            this.NOMBRE.HeaderText = "NOMBRE";
-            this.NOMBRE.Name = "NOMBRE";
-            // 
-            // IDCONSUMO
-            // 
-            this.IDCONSUMO.HeaderText = "IDCONSUMO";
-            this.IDCONSUMO.Name = "IDCONSUMO";
-            // 
-            // NOMBRES
-            // 
-            this.NOMBRES.HeaderText = "NOMBRES";
-            this.NOMBRES.Name = "NOMBRES";
-            // 
-            // IDCONSUMOS
-            // 
-            this.IDCONSUMOS.HeaderText = "IDCONSUMOS";
-            this.IDCONSUMOS.Name = "IDCONSUMOS";
+            this.button2.Location = new System.Drawing.Point(79, 503);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(283, 40);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Crear factura";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // ElegirConsumo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(453, 521);
+            this.ClientSize = new System.Drawing.Size(453, 587);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView2);
@@ -130,5 +141,6 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn NOMBRE;
         private System.Windows.Forms.DataGridViewTextBoxColumn IDCONSUMO;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
     }
 }
